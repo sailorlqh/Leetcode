dc7af30ca8913cb46cb3f2a5a06f858cabb3f14f
sailorlqh <55097418+sailorlqh@users.noreply.github.com>
2020-02-04 23:05:50 -0500

    leetcode 152

--- 152. Maximum Product Subarray.py
+++ 152. Maximum Product Subarray.py
@@ -5,20 +5,14 @@ class Solution(object):
         :rtype: int
         """
         len_num = len(nums)
-        # max_values = []
-        # min_values = []
         max_values = [0 for i in range(len_num)]
         min_values = [0 for j in range(len_num)]
-        # max_values.append(nums[0])
-        # min_values.append(nums[0])
         max_values[0] = nums[0]
         min_values[0] = nums[0]
         result = nums[0]
-        # print(result)
         for i in range(1, len_num):
         	temp1 = max_values[i - 1] * nums[i]
         	temp2 = min_values[i - 1] * nums[i]
-        	# print(temp1, temp2)
         	max_values[i] = (max(temp1, temp2, nums[i]))
         	min_values[i] = (min(temp1, temp2, nums[i]))
         	result = max(result, max_values[i])
