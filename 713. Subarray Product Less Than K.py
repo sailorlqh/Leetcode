8f77381cfc69508aeffb5e6d02f38ca6ca59cb45
sailorlqh <55097418+sailorlqh@users.noreply.github.com>
2020-02-06 12:18:05 -0500

    leetcode 713

---
+++ 713. Subarray Product Less Than K.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def numSubarrayProductLessThanK(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if(k <= 1):
+            return 0
+        count = 0
+        product = 1
+        length = len(nums)
+        left = right = 0
+        for right in range(length):
+            product *= nums[right]
+            while(product >= k):
+            	product /= nums[left]
+            	left += 1
+            count += right - left + 1
+        return count
+
+        
+sol = Solution()
+nums = [10, 5, 2, 6]
+k = 100
+print(sol.numSubarrayProductLessThanK(nums, k))
