ae178250831059469bc4bc3eca5845df9256d472
sailorlqh <55097418+sailorlqh@users.noreply.github.com>
2020-02-09 17:53:00 -0500

    update 238 with a faster version

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,4 +1,5 @@
-
+#when there is more than 1 zeros in the list
+#all the result would be zero
 class Solution(object):
     def productExceptSelf(self, nums):
         """
@@ -6,6 +7,9 @@ class Solution(object):
         :rtype: List[int]
         """
         length = len(nums)
+        nums_zero = nums.count(0)
+        if nums_zero > 1:
+            return [0] * length
         result = [0 for i in range(length)]
         result[0] = 1
         for i in range(1, length):
