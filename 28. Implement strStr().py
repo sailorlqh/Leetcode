a72cf0552f1b15234491f1ad08954d9d883069f4
sailorlqh <55097418+sailorlqh@users.noreply.github.com>
2020-02-19 22:46:53 -0500

    leetcode 27 and 28

---
+++ 28. Implement strStr().py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        length1 = len(haystack)
+        length2 = len(needle)
+        for i in range(length1 - length2 + 1):
+            if(haystack[i:i+length2] == needle):
+                return i
+        return -1
+        
