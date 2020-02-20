2afa43d93ff3a9a885bcfcb70420cd6739ff36cc
sailorlqh <55097418+sailorlqh@users.noreply.github.com>
2020-02-20 16:36:21 -0500

    Update 325. Maximum Size Subarray Sum Equals k.py

--- 325. Maximum Size Subarray Sum Equals k.py
+++ 325. Maximum Size Subarray Sum Equals k.py
@@ -5,33 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        max_length = 0
-        left = 0
-        right = 0
-        current_sum = 0
-        sum_positive = 0
-        sum_negative = 0
+        a = {}
         length = len(nums)
-        for right in range(length):
-        	current_sum = sum_negative + sum_positive + nums[right]
-        	while(current_sum > k):
-        		# current_sum -= nums[left]
-        		if(nums[left] >= 0):
-        			sum_positive -= nums[left]
-        		else:
-        			sum_negative -= nums[left]
-        		left += 1
-        		current_sum = sum_negative + sum_positive + nums[right]
-        	if(nums[right] >= 0):
-        		sum_positive += nums[right]
-        	else:
-        		sum_negative += nums[right]
-        	current_sum = sum_positive + sum_negative
-        	if(current_sum == k):
-        		max_length = max(max_length, right - left + 1)
+        max_length = 0
+        count = 0
+        for i in range(length):
+            count += nums[i]
+            if(count == k):
+                max_length = i+1
+            elif(count - k in a):
+                max_length = max(max_length, i - a[count - k])
+            if(count not in a):
+                a[count] = i
         return max_length
-
-sol = Solution()
-nums = [1, -1, 5, -2, 3]
-k = 3
-print(sol.maxSubArrayLen(nums, k))
