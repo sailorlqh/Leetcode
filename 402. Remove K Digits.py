b424f26ec2bacbeeb57b8c7e45b7d7af2e6f84d4
sailorlqh <55097418+sailorlqh@users.noreply.github.com>
2020-02-20 22:50:24 -0500

    Create 402. Remove K Digits.py

---
+++ 402. Remove K Digits.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def removeKdigits(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        length = len(num)
+        expected_length = length - k
+        if(length == k):
+            return '0'
+        count_zero = num.count('0')
+        if(count_zero + k == length):
+            return '0'
+        stack = []
+        ans = ''
+        for i in range(length):
+            if(len(stack) == 0):
+                stack.append(num[i])
+            elif(len(stack) + len(num[i:]) == expected_length):
+                ans = num[i:]
+                break
+            elif(stack[-1] > num[i]):
+                while(len(stack) != 0 and stack[-1] > num[i]):
+                    stack.pop()
+                    if(len(stack) + len(num[i:]) == expected_length):
+                        ans = num[i:]
+                        break
+                stack.append(num[i])
+            else:
+                stack.append(num[i])
+        while(len(stack) != 0):
+            ans = stack.pop() + ans
+        ans = str(int(ans))
+        if(len(ans) > expected_length):
+            ans = ans[:expected_length]
+        return ans
+            
+        
